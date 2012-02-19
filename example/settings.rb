f9030495c3c17c04fc8e769710c7e9c16db7acad
hadashi <12@hadashikick.jp>
2012-02-20 00:01:15 +0900

    fix DSL

--- example/settings.rb
+++ example/settings.rb
@@ -1,19 +1,21 @@
 # coding: utf-8
 
-message do 
-  get_point! :appearance, "おれたちの闘争本能に火が点いた！"
-  get_point! :attack_sequence_0, "今だ！みんなで袋叩きだ！"
-  get_point! :attack_sequence_1, "練習どおりに攻撃だ！"
-  get_point! :attack_sequence_2, "世界進出だ！"
-  get_point! :tanuki_deshi, "野生のタヌキが弟子になった！"
+dir '~/tmp/out/'
+
+font 'ヒラギノ明朝 Pro W3.otf', :size => 10.pt do 
+  'appearance.png'        << "おれたちの闘争本能に火が点いた！"
+  'attack_sequence_0.png' << "今だ！みんなで袋叩きだ！"
+  'attack_sequence_1.png' << "練習どおりに攻撃だ！"
+  'attack_sequence_2.png' << "世界進出だ！"
+  'tanuki_deshi.png'      << "野生のタヌキが弟子になった！"
 end
 
-label do 
-  at :score, "SCORE:"
-  at :karate_count, "おしりプリンプリン空手:"
-  at :enemy_count, "野菜ドロボウ:"
+font 'ヒラギノ角ゴ Pro W6.otf', :size => 12.pt do 
+  'score.png'        << "SCORE:"
+  'karate_count.png' << "おしりプリンプリン空手:"
+  'enemy_count.png'  << "野菜ドロボウ:"
 end
 
-effect do 
-  on :gameover, "ゲームオーバー", :size => '40pt'
+font 'ヒラギノ丸ゴ ProN W4.otf', :size => 40.pt do 
+  'gameover.png' << "ゲームオーバー"
 end
